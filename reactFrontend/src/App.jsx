c80190f515b5226d8ee24b45d00e1d4f291f2a6c
kNeff9 <kevinneff007@gmail.com>
2026-06-24 22:34:30 -0400

    Frontend skeleton

--- reactFrontend/src/App.jsx
+++ reactFrontend/src/App.jsx
@@ -1,16 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <button onClick={setCount(count + 1)}>{count}</button>
-    </>
+    <div className="home">
+      <h1 className="title">Red String News</h1>
+    </div>
   )
 }
 
