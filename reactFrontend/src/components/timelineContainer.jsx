c3b0f2dbf98036a85696b9006ae4720e52cdff7c
kNeff9 <kevinneff007@gmail.com>
2026-07-01 16:48:19 -0400

    Changes to timeline container

--- reactFrontend/src/components/timelineContainer.jsx
+++ reactFrontend/src/components/timelineContainer.jsx
@@ -1,3 +1,11 @@
-import { useState, useEffect } from "react";
 import "../css/timelineContainer.css"
 
+function TimelineContainer({ timeline }) {
+  return (
+    <article className="timeline-container">
+      <h2 className="timeline-container-title">{timeline.title}</h2>
+    </article>
+  )
+}
+
+export default TimelineContainer
